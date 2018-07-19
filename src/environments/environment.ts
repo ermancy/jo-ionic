ad8402d5867c63a430e70c2d8752f73a11e0a0e2
Erman Can Yelekçi <ermancy@gmail.com>
2018-07-20 01:19:42 +0300

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-	production: false,
-	firebase : {
-		apiKey		: "AIzaSyBJDAZ5e2h2NsDqsgeZlqWJ_eIxhR80sNY",
-		authDomain	: "job-order-f49cf.firebaseapp.com",
-		projectId	: "job-order-f49cf"
-	}
-};
